cc92fe3e48744c577ef6ff5f133772f9c3618237
Safayat Zisan <zisan_cse_mbstu@hotmail.com>
2020-08-08 23:46:54 +0600

    dtos

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Commander.Data;
+using Commander.Dtos;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,21 +14,31 @@ namespace Commander.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
         //GET api/commands
         [HttpGet]
-        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
         {
             var commandItems = _repository.GetAllCommands();
-            return Ok(commandItems);
+            return Ok(_mapper.Map< IEnumerable<CommandReadDto>>(commandItems));
         }
 
         //GET api/commands/{id}
         [HttpGet("{id}")]
-        public ActionResult<Command> GetCommandById(int id)
+        public ActionResult<CommandReadDto> GetCommandById(int id)
         {
             var commandItem = _repository.GetCommandById(id);
-            return Ok(commandItem);
+            if(commandItem != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            }
+            return NotFound();
         }
     }
 }
---
+++ Commander/Profiles/CommandsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Commander.Dtos;
+using Commander.Models;
+
+namespace Commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
